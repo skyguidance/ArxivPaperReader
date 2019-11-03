93401e67b3f93d36383a4447531ce266c013ac85
wuhang <nike0good@gmail.com>
2019-11-03 17:10:15 -0500

    Modify name&id of account fragment's elements.

--- app/src/main/java/com/example/cardviewdemo/ui/fragment/AccountFragment.kt
+++ app/src/main/java/com/example/cardviewdemo/ui/fragment/AccountFragment.kt
@@ -4,29 +4,18 @@ import android.graphics.Color
 import android.view.Gravity
 import android.view.View
 import android.widget.TextView
+import com.example.cardviewdemo.R
 import com.example.cardviewdemo.base.BaseFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class AccountFragment : BaseFragment(){
     fun newInstance(): AccountFragment {
         return AccountFragment()
     }
 
-    fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_account, container, false)
-
-    }
-
-
-
     override fun initView(): View? {
 
-        val tv = TextView(context)
-        tv.gravity = Gravity.CENTER
-        tv.setTextColor(Color.RED)
-        tv.text = javaClass.simpleName
-        return tv
+        return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
 }
