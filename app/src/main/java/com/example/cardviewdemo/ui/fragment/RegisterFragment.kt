0a75964b6303d39b6059806eb4f56497a0d997c4
wuhang <nike0good@gmail.com>
2019-11-18 08:18:37 -0500

    [register] regeister add_usr

--- app/src/main/java/com/example/cardviewdemo/ui/fragment/RegisterFragment.kt
+++ app/src/main/java/com/example/cardviewdemo/ui/fragment/RegisterFragment.kt
@@ -2,6 +2,7 @@ package com.example.cardviewdemo.ui.fragment
 
 import android.view.View
 import com.example.cardviewdemo.R
+import com.example.cardviewdemo.api.mysql.mysql_add_usr
 import com.example.cardviewdemo.base.BaseFragment
 import kotlinx.android.synthetic.main.fragment_register.view.*
 
@@ -16,8 +17,23 @@ class RegisterFragment : BaseFragment(){
         var view = View.inflate(context, R.layout.fragment_register, null)
 
         view.btn_register_register.setOnClickListener {
-            fragmentManager?.beginTransaction()?.addToBackStack(null)
-                ?.replace(R.id.container,LoginFragment())?.commit()
+
+            try {
+                Thread({
+                    var s1 = view.et_register_username!!.text.toString()
+                    var s2 = view.et_register_password!!.text.toString()
+
+                    mysql_add_usr.mysql_add_usr(s1,s2)
+
+                }).start()
+                fragmentManager?.beginTransaction()?.addToBackStack(null)
+                    ?.replace(R.id.container,LoginFragment())?.commit()
+            } catch (e: Exception) {
+
+            } finally {
+
+            }
+
         }
         view.tv_register_login.setOnClickListener {
             fragmentManager?.beginTransaction()?.addToBackStack(null)
