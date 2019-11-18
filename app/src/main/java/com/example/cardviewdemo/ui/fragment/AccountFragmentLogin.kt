76485f1d658c3513b8fb5e4f80575c6231cc9d0f
Gaozhi Liu <chrysalisliu@gwu.edu>
2019-11-18 16:42:37 -0500

    Add Login Account Fragment

---
+++ app/src/main/java/com/example/cardviewdemo/ui/fragment/AccountFragmentLogin.kt
@@ -0,0 +1,44 @@
+package com.example.cardviewdemo.ui.fragment
+
+import CreditFragement
+import FavoriteFragement
+import HistoryFragement
+import SettingsFragement
+import android.view.View
+import com.example.cardviewdemo.R
+import com.example.cardviewdemo.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_account.view.*
+
+class AccountFragmentLogin: BaseFragment() {
+    fun newInstance(): AccountFragmentLogin {
+        return AccountFragmentLogin()
+    }
+
+    override fun initView(): View? {
+
+        var view = View.inflate(context, R.layout.fragment_account_login, null)
+
+        view.btn_favorites.setOnClickListener {
+            fragmentManager?.beginTransaction()?.addToBackStack(null)
+                ?.replace(R.id.container,FavoriteFragement())?.commit()
+        }
+        view.btn_history.setOnClickListener {
+            fragmentManager?.beginTransaction()?.addToBackStack(null)
+                ?.replace(R.id.container,HistoryFragement())?.commit()
+        }
+        view.btn_settings.setOnClickListener {
+            if (isRegistered()) {
+                fragmentManager?.beginTransaction()?.addToBackStack(null)
+                    ?.replace(R.id.container,LoginFragment())?.commit()
+            }else {
+                fragmentManager?.beginTransaction()?.addToBackStack(null)
+                    ?.replace(R.id.container,SettingsFragement())?.commit()
+            }
+        }
+        view.btn_credit.setOnClickListener {
+            fragmentManager?.beginTransaction()?.addToBackStack(null)
+                ?.replace(R.id.container,CreditFragement())?.commit()
+        }
+        return view;
+    }
+}
